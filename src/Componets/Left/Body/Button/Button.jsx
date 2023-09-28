37178225145ea40efe1371388ee2883fede876aa
Metu Amarachi Cynthia <chukwuemekaokafor@Amamets-MacBook-Pro-2.local>
2023-09-28 16:18:52 +0100

    new updates

--- src/Componets/Left/Body/Button/Button.jsx
+++ src/Componets/Left/Body/Button/Button.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Button = () => {
   return (
     <div>
-      <a  href="https://google.com/" target="_blank" rel="noopener noreferrer"><button className='btn'>MY GITHUB</button></a>
-      <a  href="https://google.com/" target="_blank" rel="noopener noreferrer"><button className='btn btn_2'>LINKEDIN</button></a>
+      <a  href="https://github.com/amacyn" target="_blank" rel="noopener noreferrer"><button className='btn'>MY GITHUB</button></a>
+      <a  href="https://www.linkedin.com/in/metu-amarachi-75021224a/" target="_blank" rel="noopener noreferrer"><button className='btn btn_2'>LINKEDIN</button></a>
     </div>
   )
 }
